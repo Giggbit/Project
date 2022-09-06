0ca9ff2e61a88e355e5cfb02040fb58daefa9c4c
Giggbit <danya19981017@gmail.com>
2022-09-06 23:26:29 +0200

    Создал get и инициализировал age в main.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@ using namespace std;
 
 
 class Point {
+private:
 	int age;
+
+public:
+	Point(int a) {
+		age = a;
+	}
+	int GetPoint() {
+		return age;
+	}
+
 };
 
 
 int main() {
+	Point p(18);
 
 }
 
